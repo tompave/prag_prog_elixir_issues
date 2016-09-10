245fdcbca6dc6c743bb5206b519b38907d06bd23
tom <tommaso@pavese.me>
2016-09-10 17:21:02 +0100

    added documentation dependencies

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Issues.Mixfile do
   def project do
     [app: :issues,
      escript: escript_config,
+     name: "IssuesName",
+     source_url: "https://github.com/tompave/prag_prog_elixir_issues",
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
@@ -30,7 +32,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       httpoison: "~> 0.9.1",
-      poison: "~> 2.2"
+      poison: "~> 2.2",
+      ex_doc: "~> 0.13",
+      earmark: "~> 1.0"
     ]
   end
 
